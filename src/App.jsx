aaeeff981576c9bfe4c276c3ae5dc4bb53bdb9d2
nuex001 <edeemmanuelchizurumoke@gmail.com>
2023-09-19 13:04:55 -0700

    worked on this pfp nft , only the contract abi and address exists here

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useEthersSigner } from './utils/ethers';
+import ipfs from "./utils/Ipfs"
+import { contractABI, contractAddress } from './utils/constants'
+import { useWalletClient } from 'wagmi'
+import { FiLoader } from "react-icons/fi"
+import { ethers } from 'ethers'
+function App() {
+  const [laoding, setlaoding] = useState(0);
+  const { data: walletClient } = useWalletClient();
+  const signer = useEthersSigner();
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (walletClient) {
+        setlaoding(true);
+        const name = e.target.username.value;
+        const twitter = e.target.twitter.value;
+        const data = { name: name,
+           image: "QmNMGJbxVuQVzuhR5x4BRCb5ku2qRnSznisBeQnihnLuh3",
+           description:"AN Nft PFP for pass",
+            external_url: `https://twitter.com/${twitter}` };
+        const dataString = JSON.stringify(data);
+        const { cid } = await ipfs.add(dataString);
+        const tokenUrl = cid.toString();
+        console.log(dataString);
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        const tx = await contract.mint(tokenUrl);
+        setlaoding(false);
+        e.target.reset();
+      }
+    } catch (error) {
+      console.log(error);
+      setlaoding(false);
+    }
+  }
+
+  return (
+    <div className="container">
+      <nav>
+        <a href="/"><span>STRT</span>PFP</a>
+        <ConnectButton />
+      </nav>
+      <form action="" onSubmit={onSubmit}>
+        <input type="text" name='username' placeholder='Username' required />
+        <input type="text" name='twitter' placeholder='Twitter Username' required />
+        <button>
+          {
+            laoding ?
+              <FiLoader className='icon' />
+              :
+              "Mint"
+          }
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default App
